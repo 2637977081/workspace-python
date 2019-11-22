fc186cb1926568a829387c089303a568a5f53e3a
lvgang <lvgang@golaxy.cn>
2019-11-22 15:34:35 +0800

    修改提交

---
+++ newbie/test_image.py
@@ -0,0 +1,17 @@
+import cv2
+
+input_image_path='..\\test1\\resources\\yaojing.jpg'
+
+img_rgb = cv2.imread(input_image_path)
+#转化为灰度图
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+# 对原图进行模糊化
+img_gray = cv2.medianBlur(img_gray, 5)
+# 二值化操作
+img_edge = cv2.adaptiveThreshold(img_gray, 255,
+                                 cv2.ADAPTIVE_THRESH_MEAN_C,
+                                 cv2.THRESH_BINARY, blockSize=3, C=2)
+
+
+output_image_path='..\\test1\\resources\\yaojing1.jpg'
+cv2.imwrite(output_image_path, img_edge)
